0fca05df513aaaa99d64ef6386e523dbcaefb70d
yoseftuk <yoos@gmail.com>
2021-07-12 10:42:31 +0300

    update cookiemanager

--- src/managers/cookie.manager.ts
+++ src/managers/cookie.manager.ts
@@ -30,7 +30,7 @@ const cookieManager = {
         for (const cookie of cookies) {
             const eqPos = cookie.indexOf("=");
             const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            this.remove(name)
         }
     }
 };
