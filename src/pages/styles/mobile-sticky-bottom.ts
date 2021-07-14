a36df37bfa51bcbf6602b05515b44b782ca91afd
yoseftuk <yoos@gmail.com>
2021-07-14 10:21:12 +0300

    [revert] submit button sticky bottom

--- src/pages/styles/mobile-sticky-bottom.ts
+++ src/pages/styles/mobile-sticky-bottom.ts
@@ -2,11 +2,6 @@ import styled from "styled-components";
 
 export const MobileStickyBottom = styled.div`
 @media all and (max-width: ${p => p.theme.vars.media.tablet}px) {
-    position: sticky;
-    bottom: 0;
-    padding-bottom: 20px;
-    background: linear-gradient(to bottom, transparent 30px, white 15px);
-    margin: 0 -1px -20px 0;
-    z-index: 2;
+
 }
 `;
