135de42fee027343273ba5478f9f7202a0f50570
yoseftuk <yoos@gmail.com>
2021-07-21 22:43:05 +0300

    update toast desing

--- src/components/toast/toast.component.tsx
+++ src/components/toast/toast.component.tsx
@@ -1,27 +1,38 @@
 import React, {useState, useEffect, useRef} from 'react';
 import Styles from './toast.styles';
 import Animator from "../../hoc/animator/animator.component";
-import {Alert} from 'antd'
+import {ReactComponent as TimesIcon} from '../../assets/media/icons/times-fill.svg'
+import {ReactComponent as CheckIcon} from '../../assets/media/icons/check-fill.svg'
+import {ReactComponent as CloseIcon} from '../../assets/media/icons/times.svg'
+import {classes} from "../../pipes/classes.pipe";
 
 export type ToastProps = {type: 'success'|'error', msg: string};
-type ToastPropsInternal = ToastProps & {id: number};
+type ToastPropsInternal = ToastProps & {id: number, onClose?: ()=>void};
 let id = 0;
-let t = 0;
-export const toast = {show: (props: ToastProps) => {}, duration: 2000};
-const ToastItem = ({type, msg, idx}: ToastPropsInternal & {idx: number}) => {
+let t=0;
+export const toast = {show: (props: ToastProps) => {}, duration: 4000};
+const ToastItem = ({type, msg, idx, onClose}: ToastPropsInternal & {idx: number}) => {
     const [top, setTop] = useState(-50);
     useEffect(() => {
         setTop(idx * 50);
     }, [idx]);
     return (
         <Animator value={top} duration={300} func={Animator.SPRING(1)}>{({value}) => (
-            <Alert showIcon message={msg} type={type} style={{top: `${value}px`, right:0, position:'absolute'}}/>
+            <div className={classes('alert', `alert__${type}`)} style={{top: `${value}px`, right:0, position:'absolute'}}>
+                {type === 'error'?<TimesIcon/>:<CheckIcon/>}
+                <div className={'alert__msg'}>{msg}</div>
+                <CloseIcon className={'alert__close'} onClick={onClose}/>
+            </div>
         )}</Animator>
     );
 };
 const Toast = () => {
     const [toasts,setToasts] = useState<ToastPropsInternal[]>([]);
     const tref = useRef<ToastPropsInternal[]>([]);
+    const remove = (toastId: number) => {
+        tref.current = tref.current.filter(t => t.id !== toastId);
+        setToasts([...tref.current]);
+    };
     toast.show = (props: ToastProps) => {
         clearTimeout(t);
         t = setTimeout(() => {
@@ -29,8 +40,7 @@ const Toast = () => {
             tref.current.unshift({...props, id:toastId});
             setToasts([...tref.current]);
             setTimeout(() => {
-                tref.current = tref.current.filter(t => t.id !== toastId);
-                setToasts([...tref.current]);
+                remove(toastId);
             }, toast.duration);
         }, 400) as unknown as number;
     };
@@ -38,7 +48,7 @@ const Toast = () => {
         <Styles>
             {
                 toasts.map((t, i) => (
-                    <ToastItem {...t} idx={i} key={t.id}/>
+                    <ToastItem {...t} idx={i} key={t.id} onClose={() => remove(t.id)}/>
                 ))
             }
         </Styles>
--- src/components/toast/toast.styles.ts
+++ src/components/toast/toast.styles.ts
@@ -10,7 +10,29 @@ export default styled.div`
     flex-direction: column;
     align-items: flex-end;
     z-index: ${p => p.theme.vars.zIndex.toast};
-    .ant-alert {
+    .alert {
         margin: 5px 20px;
+        border-radius: 12px;
+        ${p => p.theme.extend.flexCenter}
+        padding: 18px;
+        &__success {
+            background-color: #EBF7EE;
+            border: 1px solid #D2EDDA;        
+        }
+        &__error {
+            background-color: #FCEDEA;
+            border: 1px solid: #F8DCD7;
+        }
+        &__msg {
+            padding: 0 38px 0 13px;
+            font-size: 16px;
+            font-weight: 500;
+            display: block;
+        }
+        &__close {
+            color: #AFAFAF;
+            width: 12px;
+            height: 12px;
+        }
     }
 `;
