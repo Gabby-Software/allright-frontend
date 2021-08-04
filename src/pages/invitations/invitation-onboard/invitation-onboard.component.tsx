3cfb71704b9829133d2886bb356f8a85b27214d7
yoseftuk <yoos@gmail.com>
2021-08-04 12:55:42 +0300

    disable skip reset password

--- src/pages/invitations/invitation-onboard/invitation-onboard.component.tsx
+++ src/pages/invitations/invitation-onboard/invitation-onboard.component.tsx
@@ -1,19 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import Styles from './invitation-onboard.styles';
 import {useIsMobile} from "../../../hooks/is-mobile.hook";
-import {OnBoardProvider} from "../../onboard/onboard.context";
+import {OnBoardContext, OnBoardProvider} from "../../onboard/onboard.context";
 import OnBoardMobile from "../../onboard/onboard.mobile";
 import OnboardDesktop from "../../onboard/onboard.desktop";
 import {Skip} from "../../styles/skip.styles";
 import {invitationOnBoardData} from "./invitation-onboard.data";
 
 type Props = {};
+const SkipOnboard = () => {
+    const {step} = useContext(OnBoardContext);
+    if(step!==0)
+        return <Skip/>;
+    return null;
+};
 const InvitationOnboard = ({}:Props) => {
     const isMobile = useIsMobile();
     return (
         <OnBoardProvider steps={invitationOnBoardData}>
             {isMobile ? <OnBoardMobile/> : <OnboardDesktop/>}
-            <Skip/>
+            <SkipOnboard/>
         </OnBoardProvider>
     );
 };
