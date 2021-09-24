bd702f9a1a2724b79670fe855aa6e85bfc5c0a81
dream-topdev <atroxltd@gmail.com>
2021-09-24 17:47:49 +0600

    change eatright login redirect

--- src/pipes/main-host.ts
+++ src/pipes/main-host.ts
@@ -2,6 +2,16 @@ export const mainHost = () => {
   if (document.location.host.startsWith('localhost')) {
     return 'http://localhost:8022'
   }
+
+  // Redirect to dashboard for EatRight
+  if (document.location.host.includes('eatright')) {
+    return (
+      document.location.protocol +
+      '//' +
+      document.location.hostname.split('.').slice(1).join('.') + '/dashboard'
+    )
+  }
+
   return (
     document.location.protocol +
     '//' +
