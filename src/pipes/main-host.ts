aae008b09302db874f487fda5659799bbcd57337
dream-topdev <atroxltd@gmail.com>
2021-10-28 23:48:08 +0600

    eatright redirect fix

--- src/pipes/main-host.ts
+++ src/pipes/main-host.ts
@@ -4,13 +4,13 @@ export const mainHost = () => {
   }
 
   // Redirect to dashboard for EatRight
-  if (document.location.host.includes('eatright')) {
-    return (
-      document.location.protocol +
-      '//' +
-      document.location.hostname.split('.').slice(1).join('.') + '/dashboard'
-    )
-  }
+  // if (document.location.host.includes('eatright')) {
+  //   return (
+  //     document.location.protocol +
+  //     '//' +
+  //     document.location.hostname.split('.').slice(1).join('.') + '/dashboard'
+  //   )
+  // }
 
   return (
     document.location.protocol +
