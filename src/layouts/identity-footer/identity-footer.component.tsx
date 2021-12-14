04c315b1e0b1db13f329ba073e6061a4b5827d1d
Ghulam213 <gmuhammad213@gmail.com>
2021-12-14 21:08:38 +0500

    add redirect link to ar footer links

--- src/layouts/identity-footer/identity-footer.component.tsx
+++ src/layouts/identity-footer/identity-footer.component.tsx
@@ -1,10 +1,17 @@
-// import config from '../../config/branding.config'
+// import config  from '../../config/branding.config'
 import { useTranslation } from '../../modules/i18n/i18n.hook'
+import { mainHost } from '../../pipes/main-host'
 import Styles from './identity-footer.styles'
 
 type Props = {}
 const IdentityFooter = ({}: Props) => {
   const { t } = useTranslation()
+
+  const ERLink = mainHost()
+  const supportLink = `${ERLink}/contacts`
+  const tncLink = `${ERLink}/terms-and-conditions`
+  const privacyLink = `${ERLink}/privacy-policy`
+
   return (
     <Styles>
       {/* <div className={'footer__copyright'}>
@@ -13,9 +20,15 @@ const IdentityFooter = ({}: Props) => {
           year: new Date().getFullYear()
         })}
       </div> */}
-      <div className={'footer__link'}>{t('footer.support')}</div>
-      <div className={'footer__link'}>{t('footer.tnb')}</div>
-      <div className={'footer__link'}>{t('footer.privacy')}</div>
+      <a href={supportLink} className={'footer__link'}>
+        {t('footer.support')}
+      </a>
+      <a href={tncLink} className={'footer__link'}>
+        {t('footer.tnb')}
+      </a>
+      <a href={privacyLink} className={'footer__link'}>
+        {t('footer.privacy')}
+      </a>
     </Styles>
   )
 }
